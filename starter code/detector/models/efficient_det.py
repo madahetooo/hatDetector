2f39d06d65a6d23c87f19d00bd40c6582986f141
Mariam <mariamabdeltawab18@gmail.com>
2020-09-16 02:43:13 +0200

    Updating model.py, run.py

--- starter code/detector/models/efficient_det.py
+++ starter code/detector/models/efficient_det.py
@@ -1,5 +1,77 @@
+import pandas as pd
+import numpy as np
+import os
+import cv2
+from PIL import Image, ImageDraw
+from ast import literal_eval
+import matplotlib.pyplot as plt
+import urllib
+from tqdm.notebook import tqdm
 import tensorflow as tf
+from keras_retinanet import models
+from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_retinanet.utils.visualization import draw_box, draw_caption
+from keras_retinanet.utils.colors import label_color
+
+train_img = "/Users/eslammedhat/WORK/Kortobaa/ML Training/starter code/data/raw/train"
+test_img = "/Users/eslammedhat/WORK/Kortobaa/ML Training/starter code/data/raw/test"
+train_csv = '/Users/eslammedhat/WORK/Kortobaa/ML Training/starter code/data/raw/train/_annotations.csv'
+sample_submission = "/Users/eslammedhat/WORK/Kortobaa/ML Training/starter code/data/raw/sample_submission.csv"
+
+with open("classes.csv", "w") as file:
+    file.write("helmet,0")
+
+    print(f"Total Bboxes: {train.shape[0]}")
+    train['width'].unique() == train['height'].unique() == [1024]
+    train['heightBB'] = train['heightBB'].astype(float)
+train['widthBB'] = train['widthBB'].astype(float)
+unique_images = train['filename'].unique()
+len(unique_images)
+num_total = len(os.listdir(train_img))
+num_annotated = len(unique_images)
+
+print(f"There are {num_annotated} annotated images and {num_total - num_annotated} images without annotations.")
+
 
 def get_model(cf):
     # TODO: build your model
-    pass
+
+    PRETRAINED_MODEL = './snapshots/_pretrained_model.h5'
+
+
+#### OPTION 1: DOWNLOAD INITIAL PRETRAINED MODEL FROM FIZYR ####
+URL_MODEL = 'https://github.com/fizyr/keras-retinanet/releases/download/0.5.0/resnet50_coco_best_v2.1.0.h5'
+import requests
+
+requests.get(URL_MODEL, PRETRAINED_MODEL)
+print('Downloaded pretrained model to ' + PRETRAINED_MODEL)
+
+model_path = os.path.join('snapshots', sorted(os.listdir('snapshots'), reverse=True)[0])
+
+model = models.load_model(model_path, backbone_name='resnet50')
+model = models.convert_model(model)
+
+pass
+
+
+def show_images(images, num=5):
+    images_to_show = np.random.choice(images, num)
+
+    for image_id in images_to_show:
+
+        image_path = os.path.join(train_img, image_id)
+        image = Image.open(image_path)
+
+        # get all bboxes for given image in [xmin, ymin, width, height]
+        bboxes = [literal_eval(box) for box in train[train['filename'] == image_id]['bbox']]
+
+        # visualize them
+        draw = ImageDraw.Draw(image)
+        for bbox in bboxes:
+            draw.rectangle(
+                [train['xmin'], train['ymin'], train['xmin'] + train['widthbb'], train['ymin'] + train['heightbb']],
+                width=3)
+
+        # plt.figure(figsize = (15,15))
+        # plt.imshow(image)
+        # plt.show().
--- starter code/run.py
+++ starter code/run.py
@@ -3,42 +3,130 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from detector.data.data_loader import get_dataset
-from detector.models.efficient_det import get_model
-from detector.utils.exp_utils import prepare_experiment
-from detector.utils.model_utils import create_submission
+from data.data_loader import get_dataset
+from models.efficient_det import get_model
+from utils.exp_utils import prepare_experiment
+from utils.model_utils import create_submission
+
 
 def train(cf):
-    # Write your training script here
 
-    # TODO: Create the datasets from data/dataloader.py
-    train_dataset = get_dataset(cf, mode="train")
-    val_dataset = get_dataset(cf, mode="validation")
 
-    # TODO: Create tensorboard and model_saver callbacks
+# Write your training script here
 
-    # TODO: Build your model and load weights if you are resuming training
 
-    # TODO: Compile and train your model
+model_path = os.path.join('snapshots', sorted(
+    os.listdir('snapshots'), reverse=True)[0])
 
-    pass
+model = models.load_model(model_path, backbone_name='resnet50')
+model = models.convert_model(model)
+
+# TODO: Create the datasets from data/dataloader.py
+train_dataset = get_dataset(cf, mode="train")
+val_dataset = get_dataset(cf, mode="validation")
+
+# TODO: Create tensorboard and model_saver callbacks
+
+# TODO: Build your model and load weights if you are resuming training
+
+# TODO: Compile and train your model
+
+pass
+
 
 def test(cf):
-    # Write the test script here
-
-    # TODO: Create the dataloader from data/dataloader.py
-    dataset = get_dataset(cf, mode="test")
-    # TODO: Load your saved model
-
-    # TODO: Run prediction
-
-    # TODO: Create the submission.csv file (it's better to do it in 
-    # utils/model_utils.py) and call it here.
-    create_submission(results)
-    pass
 
 
-if __name__ == "__main__":
+# Write the test script here
+
+
+def predict(image):
+    image = preprocess_image(image.copy())
+    # image, scale = resize_image(image)
+
+    boxes, scores, labels = model.predict_on_batch(
+        np.expand_dims(image, axis=0)
+    )
+
+    # boxes /= scale
+
+    return boxes, scores, labels
+    THRES_SCORE = 0.5
+
+
+def draw_detections(image, boxes, scores, labels):
+    for box, score, label in zip(boxes[0], scores[0], labels[0]):
+        if score < THRES_SCORE:
+            break
+
+        color = label_color(label)
+
+        b = box.astype(int)
+        draw_box(image, b, color=color)
+
+        caption = "{:.3f}".format(score)
+        draw_caption(image, b, caption)
+
+        def show_detected_objects(image_name):
+    img_path = test_img + '/' + image_name
+
+    image = read_image_bgr(img_path)
+
+    boxes, scores, labels = predict(image)
+    print(boxes[0, 0].shape)
+    draw = image.copy()
+    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+    draw_detections(draw, boxes, scores, labels)
+    # plt.figure(figsize=(15,10))
+    # plt.axis('off')
+    # plt.imshow(draw)
+    # plt.show()
+
+    for img in li:
+        show_detected_objects(img)
+    preds = []
+
+
+imgid = []
+for img in tqdm(li, total=len(li)):
+    img_path = test_img + '/' + img
+    image = read_image_bgr(img_path)
+    boxes, scores, labels = predict(image)
+    boxes = boxes[0]
+    scores = scores[0]
+    for idx in range(boxes.shape[0]):
+        if scores[idx] > THRES_SCORE:
+            box, score = boxes[idx], scores[idx]
+            imgid.append(img.split(".")[0])
+            preds.append("{} {} {} {} {}".format(score, int(box[0]), int(
+                box[1]), int(box[2] - box[0]), int(box[3] - box[1])))
+            preds[0]
+            sub = {"image_id": imgid, "PredictionString": preds}
+sub = pd.DataFrame(sub)
+sub.head()
+
+sub_ = sub.groupby(["image_id"])['PredictionString'].apply(lambda x: ' '.join(x)).reset_index()
+sub_
+
+for idx, imgid in enumerate(samsub['image_id']):
+    samsub.iloc[idx, 1] = sub_[sub_['image_id'] == imgid].values[0, 1]
+
+samsub.head()
+
+# TODO: Create the dataloader from data/dataloader.py
+dataset = get_dataset(cf, mode="test")
+# TODO: Load your saved model
+
+# TODO: Run prediction
+
+# TODO: Create the submission.csv file (it's better to do it in
+# utils/model_utils.py) and call it here.
+create_submission(results)
+samsub.to_csv('/kaggle/working/submission.csv', index=False)
+pass
+
+if _name_ == "_main_":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -72,14 +160,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Current directory
-    SOURCE_DIR = os.path.dirname(os.path.realpath(__file__))
-    
-    # TODO: 
+    SOURCE_DIR = os.path.dirname(os.path.realpath(_file_))
 
+    # TODO:
 
     if args.mode == "train":
-        # TODO: Load your config file from exp_source and copy it in 
-        # exp_results and prepare the necessary folders for the experiment 
+        # TODO: Load your config file from exp_source and copy it in
+        # exp_results and prepare the necessary folders for the experiment
         # for e.g.:
         # create the folder for tensorboard logs and a folder for checkpoints
         # it's better to do this in utils/exp_utils.py and call it here
@@ -87,7 +174,7 @@ if __name__ == "__main__":
         train(cf)
 
     elif args.mode == "test":
-        # TODO: Load your config file from exp_results and prepare the 
+        # TODO: Load your config file from exp_results and prepare the
         # necessary folders for the experiment for e.g.:
         # create the folder for plotting random images with bounding boxes
         # it's better to do this in utils/exp_utils.py and call it here
